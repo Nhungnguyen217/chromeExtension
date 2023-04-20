a161e55a9266425566c955d8f4b4e90746642b46
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-04-20 09:55:44 +0700

    10

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let inputBtn = document.getElementById("input-btn");
-inputBtn.addEventListener("click", function(){
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+let myLeads =[];
+
+// inputEl -> should be assigned to the text input field
+let inputEl = document.getElementById("input-el");
+
+let saveBtn = document.getElementById("input-btn");
+saveBtn.addEventListener("click", function(){
     alert("hi")
 })
