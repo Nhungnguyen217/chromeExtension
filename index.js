4ed22924c9e0b41ff633b03e79ac369e23aadcdc
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-04-25 20:45:02 +0700

    push the value from input field

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 let myLeads =[];
 
 // inputEl -> should be assigned to the text input field
-let inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
 
-let saveBtn = document.getElementById("input-btn");
-saveBtn.addEventListener("click", function(){
-    alert("hi")
+const inputBtn = document.getElementById("input-btn")
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+inputBtn.addEventListener("click", function() {
+     // Push the value from the inputEl into the myLeads array 
+     let inputValue = document.getElementById("input-el").value;
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputValue)
+    console.log(myLeads)
 })
