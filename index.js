8d04d898e38d29f8ec3e4c745c1284500d2f7bf2
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-04-30 18:05:29 +0700

    33

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+
+localStorage.setItem("hi","www.example.com")
+// console.log(localStorage.getItem("myLeads"))
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
