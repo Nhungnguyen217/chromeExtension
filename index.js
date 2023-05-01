b67cdd61d5b2753420db75bc3de1b916e12f772f
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-05-01 11:52:05 +0700

    39

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-//edge://extensions/
-
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-var leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+// let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// console.log(leadsFromLocalStorage)
+// if(leadsFromLocalStorage){
+//   myLeads = leadsFromLocalStorage
+//   renderLeads()
+// }
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
   localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  renderLeads();
-
-  console.log(localStorage.getItem("myLeads"))
+  renderLeads()
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems +="<li> <a target='_blank' href = '" + myLeads[i] + "' >" + myLeads[i] + "</a> </li>";
     listItems += `
      <li>
       <a target='_blank' href = '${myLeads[i]}'>
